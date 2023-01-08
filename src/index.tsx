189ecbc2722087f1cd0d561d15330701bc33fddb
pjoshi1616 <pjoshi@mphrx.com>
2023-01-08 19:19:49 +0530

    codeX1616-HTML changes for clock app

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,37 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-function tick() {
+const deg: number = 6;
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+
+let day = new Date();
+let hh = day.getHours();
+let mm = day.getMinutes();
+let ss = day.getSeconds();
+
+
+
+// function tick() {
   const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
+    <>
+      {/* <h1>Welcome!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2> */}
+      <div className='clock'>
+        <div className='hour'>
+          <div className='hr' id='hr'/>
+        </div>
+        <div className='min'>
+          <div className='mn' id='mn'/>
+        </div>
+        <div className='sec'>
+          <div className='sc' id='sc'/>
+        </div>  
+      </div>
+    </>
   );
   root.render(element);
-}
+// }
 
-setInterval(tick, 1000);
+// setInterval(tick, 1000);
